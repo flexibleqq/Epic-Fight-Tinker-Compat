a0da6d5152efb0e676aac69e90380f6a8f0d76d8
minhhjj5678 <minhhjj5678@gmail.com>
2026-03-17 05:23:43 +0700

    add more part

--- src/main/java/com/minhhjjj/epicfighttinkercompat/EpicFightStatDefinitions.java
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/EpicFightStatDefinitions.java
@@ -5,13 +5,20 @@ import slimeknights.tconstruct.library.materials.stats.MaterialStatsId;
 public final class EpicFightStatDefinitions {
     private EpicFightStatDefinitions() {}
 
-    // Standard TConstruct part stat IDs used by tool parts
     public static final MaterialStatsId PART_HEAD = new MaterialStatsId(EpicFightTinkerCompat.MODID, "head");
     public static final MaterialStatsId PART_HANDLE = new MaterialStatsId(EpicFightTinkerCompat.MODID, "handle");
     public static final MaterialStatsId PART_BINDING = new MaterialStatsId(EpicFightTinkerCompat.MODID, "binding");
 
-    // JSON keys under data/<namespace>/tinkering/materials/stats/<material>.json -> stats {}
     public static final String JSON_HEAD_KEY = EpicFightTinkerCompat.MODID + ":head";
     public static final String JSON_HANDLE_KEY = EpicFightTinkerCompat.MODID + ":handle";
     public static final String JSON_BINDING_KEY = EpicFightTinkerCompat.MODID + ":binding";
+
+    public static final String JSON_HELMET_KEY = EpicFightTinkerCompat.MODID + ":plating_helmet";
+    public static final String JSON_CHESTPLATE_KEY = EpicFightTinkerCompat.MODID + ":plating_chestplate";
+    public static final String JSON_LEGGINS_KEY = EpicFightTinkerCompat.MODID + ":plating_leggings";
+    public static final String JSON_BOOTS_KEY = EpicFightTinkerCompat.MODID + ":plating_boots";
+    public static final String JSON_MAILLE_KEY = EpicFightTinkerCompat.MODID + ":maille";
+    public static final String JSON_CUIRASS_KEY = EpicFightTinkerCompat.MODID + ":cuirass";
+    public static final String JSON_SKULL_KEY = EpicFightTinkerCompat.MODID + ":skull";
+
 }
