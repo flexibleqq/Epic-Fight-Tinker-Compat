73ab71c99c7eafb71c0ba113c462a56821a7a313
minhhjj5678 <minhhjj5678@gmail.com>
2026-03-17 05:38:52 +0700

    feat: add Epic Fight armor stats to Tconstruct

--- src/main/java/com/minhhjjj/epicfighttinkercompat/EpicFightToolStats.java
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/EpicFightToolStats.java
@@ -7,17 +7,18 @@ import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
 public class EpicFightToolStats {
 
-    // Khởi tạo trực tiếp không qua Forge DeferredRegister
     public static final FloatToolStat IMPACT = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "impact"), 0xFF5555, 0f, -100f, 100f, TinkerTags.Items.MODIFIABLE);
-        
     public static final FloatToolStat MAX_STRIKES = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "max_strikes"), 0x55FF55, 0f, -100f, 100f, TinkerTags.Items.MODIFIABLE);
-        
     public static final FloatToolStat ARMOR_NEGATION = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "armor_negation"), 0x5555FF, 0f, -100f, 100f, TinkerTags.Items.MODIFIABLE);
 
-    // Đăng ký trực tiếp vào hệ thống của Tinkers
+    public static final FloatToolStat WEIGHT = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "weight"), 0x7A40D6, 0f, -100f, 100f, TinkerTags.Items.WORN_ARMOR);
+    public static final FloatToolStat STUN_ARMOR = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "stun_armor"), 0x5E40D6, 0f, -100f, 100f, TinkerTags.Items.WORN_ARMOR);
+
     public static void register() {
         ToolStats.register(IMPACT);
         ToolStats.register(MAX_STRIKES);
         ToolStats.register(ARMOR_NEGATION);
+        ToolStats.register(WEIGHT);
+        ToolStats.register(STUN_ARMOR);
     }
 }
