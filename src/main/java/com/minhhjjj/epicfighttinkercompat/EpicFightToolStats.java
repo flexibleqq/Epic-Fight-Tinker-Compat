3e0cc3bc6f1365265d36488fff145dab55ddf00e
minhhjj5678 <minhhjj5678@gmail.com>
2026-03-20 21:36:00 +0700

    feat: increase tool stats cap

--- src/main/java/com/minhhjjj/epicfighttinkercompat/EpicFightToolStats.java
+++ src/main/java/com/minhhjjj/epicfighttinkercompat/EpicFightToolStats.java
@@ -7,12 +7,12 @@ import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
 public class EpicFightToolStats {
 
-    public static final FloatToolStat IMPACT = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "impact"), 0xFF5555, 0f, -100f, 100f, TinkerTags.Items.MODIFIABLE);
-    public static final FloatToolStat MAX_STRIKES = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "max_strikes"), 0x55FF55, 0f, -100f, 100f, TinkerTags.Items.MODIFIABLE);
-    public static final FloatToolStat ARMOR_NEGATION = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "armor_negation"), 0x5555FF, 0f, -100f, 100f, TinkerTags.Items.MODIFIABLE);
+    public static final FloatToolStat IMPACT = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "impact"), 0xFF5555, 0f, -1024f, 1024f, TinkerTags.Items.MODIFIABLE);
+    public static final FloatToolStat MAX_STRIKES = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "max_strikes"), 0x55FF55, 0f, -1024f, 1024f, TinkerTags.Items.MODIFIABLE);
+    public static final FloatToolStat ARMOR_NEGATION = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "armor_negation"), 0x5555FF, 0f, -1024f, 1024f, TinkerTags.Items.MODIFIABLE);
 
-    public static final FloatToolStat WEIGHT = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "weight"), 0x7A40D6, 0f, -100f, 100f, TinkerTags.Items.WORN_ARMOR);
-    public static final FloatToolStat STUN_ARMOR = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "stun_armor"), 0x5E40D6, 0f, -100f, 100f, TinkerTags.Items.WORN_ARMOR);
+    public static final FloatToolStat WEIGHT = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "weight"), 0x7A40D6, 0f, -1024f, 1024f, TinkerTags.Items.WORN_ARMOR);
+    public static final FloatToolStat STUN_ARMOR = new FloatToolStat(new ToolStatId(EpicFightTinkerCompat.MODID, "stun_armor"), 0x5E40D6, 0f, -1024f, 1024f, TinkerTags.Items.WORN_ARMOR);
 
     public static void register() {
         ToolStats.register(IMPACT);
